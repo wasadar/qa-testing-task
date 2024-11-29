e2528c8154fe564bb2e7125f39d7ca3246d907ec
unknown <wasadar2@gmail.com>
2024-11-29 14:35:29 +0200

    finished task

--- test/task.integration-test-cases.ts
+++ test/task.integration-test-cases.ts
@@ -1,6 +1,7 @@
 export const TASK_TEST_SCENARIOS = {
     CREATE_SUCCESS: {
         type: "success",
+        id: 0,
         input: {
             name: "Test Name",
             description: "Test Description"
@@ -14,6 +15,7 @@ export const TASK_TEST_SCENARIOS = {
     },
     CREATE_FAIL: {
         type: "error",
+        id: 0,
         input: {
             name: "No Description"
         },
@@ -24,4 +26,97 @@ export const TASK_TEST_SCENARIOS = {
             statusCode: 400
         }
     },
+    GET_ALL_SUCCESS: {
+        type: "success",
+        id: 0,
+        input: {},
+        expectedStatus: 200,
+        expectedResponse: [{
+            id: 1,
+            name: "Test Name",
+            description: "Test Description"
+        }]
+    },
+    GET_ALL_FAIL: {
+        type: "error",
+        id: 0,
+        input: { 
+            somothing_in_the_body: "something in the body"
+        },
+        expectedStatus: 200,
+        expectedResponse: [{
+            id: 1,
+            name: "Test Name",
+            description: "Test Description"
+        }]
+    },
+    GET_SUCCESS: {
+        type: "success",
+        id: 1,
+        input: {},
+        expectedStatus: 200,
+        expectedResponse: {
+            id: 1,
+            name: "Test Name",
+            description: "Test Description"
+        }
+    },
+    GET_FAIL: {
+        type: "error",
+        id: 10,
+        input: {},
+        expectedStatus: 404,
+        expectedResponse: {
+            message: 'Item with id 10 not found',
+            error: 'Not Found',
+            statusCode: 404
+        }
+    },
+    PUT_SUCCESS: {
+        type: "success",
+        id: 1,
+        input: {
+            name: "Other Test Name",
+            description: "Other Test Description"
+        },
+        expectedStatus: 200,
+        expectedResponse: {
+            id: 1,
+            name: "Other Test Name",
+            description: "Other Test Description"
+        }
+    },
+    PUT_FAIL: {
+        type: "error",
+        id: 10,
+        input: {
+            name: "No Description"
+        },
+        expectedStatus: 400,
+        expectedResponse: {
+            message: [ 'description should not be empty' ],
+            error: 'Bad Request',
+            statusCode: 400
+        }
+    },
+    DELETE_SUCCESS: {
+        type: "success",
+        id: 1,
+        input: {},
+        expectedStatus: 200,
+        expectedResponse: {
+            message: 'Item with id 1 deleted successfully',
+        }
+    },
+    DELETE_FAIL: {
+        type: "error",
+        id: 10,
+        input: {},
+        expectedStatus: 404,
+        expectedResponse: {
+            message: 'Item with id 10 not found',
+            error: 'Not Found',
+            statusCode: 404
+        }
+    }
 }
--- test/task.integration.spec.ts
+++ test/task.integration.spec.ts
@@ -4,6 +4,11 @@ import * as request from 'supertest';
 import { TASK_TEST_SCENARIOS } from './task.integration-test-cases';
 import { TaskModule } from '../src/task.module';
 
+const new_item_data = {
+  name: "Test Name",
+  description: "Test Description"
+};
+
 describe('Task Integration Tests', () => {
   let app: INestApplication;
 
@@ -33,11 +38,91 @@ describe('Task Integration Tests', () => {
       .expect(scenario.expectedResponse);
   }
 
+  const testGetAllEndpointScenario = async (scenarioKey: keyof typeof TASK_TEST_SCENARIOS) => {
+    const scenario = TASK_TEST_SCENARIOS[scenarioKey];
+    await request(app.getHttpServer())
+      .post('/items')
+      .send(new_item_data);
+    await request(app.getHttpServer())
+      .get('/items')
+      .send(scenario.input)
+      .expect(scenario.expectedStatus)
+      .expect(scenario.expectedResponse);
+  }
+
+  const testGetEndpointScenario = async (scenarioKey: keyof typeof TASK_TEST_SCENARIOS) => {
+    const scenario = TASK_TEST_SCENARIOS[scenarioKey];
+    await request(app.getHttpServer())
+      .post('/items')
+      .send(new_item_data);
+    await request(app.getHttpServer())
+      .get(`/items/${scenario.id}`)
+      .send()
+      .expect(scenario.expectedStatus)
+      .expect(scenario.expectedResponse);
+  }
+
+  const testPutEndpointScenario = async (scenarioKey: keyof typeof TASK_TEST_SCENARIOS) => {
+    const scenario = TASK_TEST_SCENARIOS[scenarioKey];
+    await request(app.getHttpServer())
+      .post('/items')
+      .send(new_item_data);
+    await request(app.getHttpServer())
+      .put(`/items/${scenario.id}`)
+      .send(scenario.input)
+      .expect(scenario.expectedStatus)
+      .expect(scenario.expectedResponse);
+  }
+
+  const testDeleteEndpointScenario = async (scenarioKey: keyof typeof TASK_TEST_SCENARIOS) => {
+    const scenario = TASK_TEST_SCENARIOS[scenarioKey];
+    await request(app.getHttpServer())
+      .post('/items')
+      .send(new_item_data);
+    await request(app.getHttpServer())
+      .delete(`/items/${scenario.id}`)
+      .send()
+      .expect(scenario.expectedStatus)
+      .expect(scenario.expectedResponse);
+  }
+
   it('CREATE_SUCCESS', async () => {
     await testCreateEndpointScenario("CREATE_SUCCESS")
   });
 
   it('CREATE_FAIL', async () => {
     await testCreateEndpointScenario("CREATE_FAIL")
+  });
+
+  it('GET_ALL_SUCCESS', async () => {
+    await testGetAllEndpointScenario("GET_ALL_SUCCESS")
+  });
+
+  it('GET_ALL_FAIL', async () => {
+    await testGetAllEndpointScenario("GET_ALL_FAIL")
+  });
+
+  it('GET_SUCCESS', async () => {
+    await testGetEndpointScenario("GET_SUCCESS");
+  });
+
+  it('GET_FAIL', async () => {
+    await testGetEndpointScenario("GET_FAIL");
+  });
+
+  it('PUT_SUCCESS', async () => {
+    await testPutEndpointScenario("PUT_SUCCESS");
+  });
+
+  it('PUT_FAIL', async () => {
+    await testPutEndpointScenario("PUT_FAIL");
+  });
+
+  it('DELETE_SUCCESS', async () => {
+    await testDeleteEndpointScenario("DELETE_SUCCESS");
+  });
+
+  it('DELETE_FAIL', async () => {
+    await testDeleteEndpointScenario("DELETE_FAIL");
   });
 });
